8350a18bbba509e52895336232e099d0859b4cfd
Rayane MOULA <95938506+SlevinKl@users.noreply.github.com>
2023-10-28 12:06:05 +0200

    create reusable label component

---
+++ src/app/components/label/label.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-label',
+  templateUrl: './label.component.html',
+  styleUrls: ['./label.component.scss']
+})
+export class LabelComponent {
+  @Input({ required: true }) inputTitle!: string;
+  @Input({ required: true }) inputContent!: number;
+}
